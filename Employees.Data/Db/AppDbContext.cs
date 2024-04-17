3dba0a796f6efee13a13254a1afb008494c2017b
Dmytro Vyshnvskyi <vishnevsky.dmtr@gmail.com>
2024-04-17 19:39:25 +0300

    add dbContext

---
+++ Employees.Data/Db/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Employees.Data.Db.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Data.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        AppDbContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ Employees.Data/Db/Entities/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Data.Db.Entities
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        [Required]
+        public DateTime BirthDate {  get; set; }
+        [Required]
+        public string Position { get; set; }
+        public string Phone { get; set; }
+        [Required]
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set;}
+
+    }
+}
